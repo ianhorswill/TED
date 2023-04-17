e8ae5af12beac5d35ebf0a34eb5911aed737e458
Ian Horswill <ian@northwestern.edu>
2023-04-17 00:31:14 -0500

    Initial implementation of declarative column update

---
+++ TED/Interpreter/ColumnUpdater.cs
@@ -0,0 +1,27 @@
+﻿using TED.Tables;
+
+namespace TED.Interpreter
+{
+    internal abstract class ColumnUpdater
+    {
+        public abstract void DoUpdates();
+    }
+
+    internal class ColumnUpdater<TColumn, TKey> : ColumnUpdater
+    {
+        public ColumnAccessor<TColumn, TKey> Accessor;
+        public TablePredicate<TKey, TColumn> UpdateList;
+
+        public ColumnUpdater(ColumnAccessor<TColumn, TKey> accessor, TablePredicate<TKey, TColumn> updateList)
+        {
+            Accessor = accessor;
+            UpdateList = updateList;
+        }
+
+        public override void DoUpdates()
+        {
+            foreach (var (key, value) in UpdateList)
+                Accessor[key] = value;
+        }
+    }
+}
